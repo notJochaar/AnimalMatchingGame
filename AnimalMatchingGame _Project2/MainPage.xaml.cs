1a12c64dc7a09e5deb58db8d54edb24e8c5227d0
notJochaar <116516125+notJochaar@users.noreply.github.com>
2025-08-10 21:21:53 +0400

    added functionality

--- AnimalMatchingGame _Project2/MainPage.xaml.cs
+++ AnimalMatchingGame _Project2/MainPage.xaml.cs
@@ -7,6 +7,7 @@
             InitializeComponent();
         }
 
+
         private void PlayAgainButton_Clicked(object sender, EventArgs e)
         {
             AnimalButtons.IsVisible = true;
@@ -24,11 +25,49 @@
                 button.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
             }
+
+           // Dispatcher.StartTimer(TimeSpan.FromSeconds(.1), TimerTick);
         }
+
+        Button LastClicked;
+        int MatchesFound;
+        bool FindingMatch = false;
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            
+            if(sender is Button clickedButton)
+            {
+                
+
+                if (!string.IsNullOrWhiteSpace(clickedButton.Text) && (FindingMatch == false))
+                {
+                    clickedButton.BackgroundColor = Colors.Red;
+                    LastClicked = clickedButton;
+                    FindingMatch = true;
+                }
+                else
+                {
+                    if (clickedButton != LastClicked && (clickedButton.Text == LastClicked.Text )
+                        && !string.IsNullOrWhiteSpace(clickedButton.Text))
+                    {
+                        MatchesFound++;
+                        MatchesFoundLabel.Text = $"Matches Found: {MatchesFound}";
+                        LastClicked.Text = string.Empty;
+                        clickedButton.Text = string.Empty;
+                    }
+                    LastClicked.BackgroundColor = Colors.LightBlue;
+                    clickedButton.BackgroundColor = Colors.LightBlue;
+                    FindingMatch = false;
+                }
+
+                if (MatchesFound == 8)
+                {
+                    AnimalButtons.IsVisible = false;
+                    PlayAgainButton.IsVisible = true;
+                    MatchesFound = 0;
+                }
+            }
+
         }
     }
 
